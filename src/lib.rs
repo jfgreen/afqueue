f83ecf504c04a765e09e40a4eb61e91ec9cce508
James Green <1288679+jfgreen@users.noreply.github.com>
2022-04-07 21:00:06 +0100

    Introduce AudioFile abstraction to wrap framework FFI calls

--- src/lib.rs
+++ src/lib.rs
@@ -49,93 +49,126 @@ impl fmt::Display for PlaybackError {
     }
 }
 
+//struct PlaybackState {
+//playing_file, system::AudioFileID,
+//current_packet: u64, // TODO: Does this have to be i64?
+//bytes_to_read: u32,
+//packets_to_read: u32,
+//packet_descriptions: *const AudioStreamPacketDescription,
+//boolean: finished,
+//}
+
+struct AudioFile {
+    file_id: system::AudioFileID,
+}
+
+impl AudioFile {
+    fn open(path: String) -> Result<AudioFile, PlaybackError> {
+        if path.is_empty() {
+            return Err(PlaybackError::PathIsEmpty);
+        }
+
+        let path = CString::new(path)?;
+        let path = path.as_bytes();
+
+        unsafe {
+            // Create URL
+            let url_ref = system::cfurl_create_from_filesystem_representation(
+                ptr::null(), // Use default allocator
+                path.as_ptr(),
+                path.len() as isize,
+                false, // Not a directory
+            );
+
+            // Create file
+            let mut file_id = MaybeUninit::uninit();
+            let status = system::audio_file_open_url(
+                url_ref,
+                system::AUDIO_FILE_READ_PERMISSION,
+                0, // No file hints
+                file_id.as_mut_ptr(),
+            );
+
+            // Dont need the CFURL anymore
+            system::cf_release(url_ref as *const c_void);
+
+            if status != 0 {
+                return Err(PlaybackError::FailedToOpenAudioFile(status));
+            }
+
+            let file_id = file_id.assume_init();
+            Ok(AudioFile { file_id })
+        }
+    }
+
+    //TODO: Return properties instead
+    fn print_properties(&self) -> Result<(), PlaybackError> {
+        unsafe {
+            // Obtain a reference to the properties dictionary
+            let mut info_dict = MaybeUninit::<system::CFDictionaryRef>::uninit();
+            let mut dict_ref_size = mem::size_of::<system::CFDictionaryRef>() as u32;
+
+            let status = system::audio_file_get_property(
+                self.file_id,
+                system::AUDIO_FILE_PROPERTY_INFO_DICTIONARY,
+                &mut dict_ref_size as *mut _,
+                info_dict.as_mut_ptr() as *mut c_void,
+            );
+            let info_dict = info_dict.assume_init();
+            if status != 0 {
+                return Err(PlaybackError::FailedToReadFileProperty(status));
+            }
+
+            // Extract keys and values
+            let count = system::cfdictionary_get_count(info_dict);
+            let mut keys = Vec::<system::CFStringRef>::with_capacity(count as usize);
+            let mut values = Vec::<system::CFStringRef>::with_capacity(count as usize);
+            system::cfdictionary_get_keys_and_values(
+                info_dict,
+                keys.as_mut_ptr() as *mut *const c_void,
+                values.as_mut_ptr() as *mut *const c_void,
+            );
+
+            keys.set_len(count as usize);
+            values.set_len(count as usize);
+
+            // Convert to Rust strings
+            println!("keys:");
+            for k in keys {
+                let s = cfstring_to_string(k);
+                println!("{s}");
+            }
+
+            println!("\nvalues:");
+            for v in values {
+                let s = cfstring_to_string(v);
+                println!("{s}");
+            }
+
+            //TODO: Do we also have to release the contents of the dictionary?
+            system::cf_release(info_dict as *const c_void);
+
+            println!("count: {count}");
+        }
+
+        Ok(())
+    }
+
+    fn close(self) -> Result<(), PlaybackError> {
+        unsafe {
+            let status = system::audio_file_close(self.file_id);
+            if status != 0 {
+                return Err(PlaybackError::FailedToCloseAudioFile(status));
+            }
+        }
+        Ok(())
+    }
+}
+
 pub fn play(path: String) -> Result<(), PlaybackError> {
-    //TODO: Make this code more ideomatic with a newtype pattern?
-    if path.is_empty() {
-        return Err(PlaybackError::PathIsEmpty);
-    }
-
-    let path = CString::new(path)?;
-    let path = path.as_bytes();
-
-    unsafe {
-        // Create URL
-        let url_ref = system::cfurl_create_from_filesystem_representation(
-            ptr::null(),
-            path.as_ptr(),
-            path.len() as isize,
-            false,
-        );
-
-        // Create file
-        let mut audio_file_id = MaybeUninit::uninit();
-        let status = system::audio_file_open_url(
-            url_ref,
-            system::AUDIO_FILE_READ_PERMISSION,
-            0, // No file hints
-            audio_file_id.as_mut_ptr(),
-        );
-
-        //TODO: Make this work on drop?
-        system::cf_release(url_ref as *const c_void);
-        if status != 0 {
-            return Err(PlaybackError::FailedToOpenAudioFile(status));
-        }
-        let audio_file_id = audio_file_id.assume_init();
-
-        // Read file properties
-        let mut info_dict = MaybeUninit::<system::CFDictionaryRef>::uninit();
-        let mut dict_ref_size = mem::size_of::<system::CFDictionaryRef>() as u32;
-
-        let status = system::audio_file_get_property(
-            audio_file_id,
-            system::AUDIO_FILE_PROPERTY_INFO_DICTIONARY,
-            &mut dict_ref_size as *mut _,
-            info_dict.as_mut_ptr() as *mut c_void,
-        );
-        let info_dict = info_dict.assume_init();
-        if status != 0 {
-            return Err(PlaybackError::FailedToReadFileProperty(status));
-        }
-
-        let count = system::cfdictionary_get_count(info_dict);
-        let mut keys = Vec::<system::CFStringRef>::with_capacity(count as usize);
-        let mut values = Vec::<system::CFStringRef>::with_capacity(count as usize);
-        system::cfdictionary_get_keys_and_values(
-            info_dict,
-            //TODO: Can we do better than this?
-            keys.as_mut_ptr() as *mut *const c_void,
-            values.as_mut_ptr() as *mut *const c_void,
-        );
-
-        keys.set_len(count as usize);
-        values.set_len(count as usize);
-
-        println!("keys:");
-        for k in keys {
-            let s = cfstring_to_string(k);
-            println!("{s}");
-        }
-
-        //TODO: Do we know that the values are always strings?
-        println!("\nvalues:");
-        for v in values {
-            let s = cfstring_to_string(v);
-            println!("{s}");
-        }
-
-        //TODO: Do we have to release the contents of the dictionary?
-        println!("count: {count}");
-
-        system::cf_release(info_dict as *const c_void);
-
-        // Close file
-        let status = system::audio_file_close(audio_file_id);
-        if status != 0 {
-            return Err(PlaybackError::FailedToCloseAudioFile(status));
-        }
-    }
-
+    let audio_file = AudioFile::open(path)?;
+    audio_file.print_properties()?;
+    audio_file.close()?;
     Ok(())
 }
 
@@ -152,9 +185,9 @@ unsafe fn cfstring_to_string(cfstring: system::CFStringRef) -> String {
         },
         system::CFSTRING_ENCODING_UTF8,
         0,               // no loss byte
-        false,           // not external representation
+        false,           // no byte order marker
         ptr::null_mut(), // dont actually capture any bytes
-        0,               // buffer size
+        0,               // buffer size of 0 as no buffer supplied
         &mut bytes_required,
     );
 
@@ -169,8 +202,8 @@ unsafe fn cfstring_to_string(cfstring: system::CFStringRef) -> String {
             length: string_len,
         },
         system::CFSTRING_ENCODING_UTF8,
-        0,                               // no loss byte
-        false,                           // not external representation
+        0,     // no loss byte
+        false, // no byte order marker
         buffer.as_mut_ptr(),
         buffer.len() as system::CFIndex,
         &mut bytes_written,
